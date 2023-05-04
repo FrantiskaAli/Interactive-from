222f87cf925ed0e5b46e517d2063b34dd1a39188
FrantiskaAli <tiskarechk@gmail.com>
2023-05-04 22:56:56 +0100

    in this commit I finalised JS functions and styled thank you page, only left is media querries

--- script.js
+++ script.js
@@ -1,63 +1,94 @@
 console.log("I am linked properly");
 //ONCHANGE FUNCTION TO UPDATE CARD DETAILS
 /*Cardholder name input*/
-$(document).on('change', '#cardholder', function(event) {
-  $(event.target).next().remove();
-  $(event.target).removeClass("error-border");
-   typeOnCard(event.target,$('#name-on-card'))
-   if (/[0-9-`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/.test(event.target.value)) {
-    const errMsg = $('<label>').addClass('error-message').text('Enter name as on your card');
-    $(event.target).after(errMsg)
-    $(event.target).addClass("error-border");
-  }
-  });
+$(document).on('change', '#cardholder', function (event) {
+  typeOnCard(event.target, $('#name-on-card'))
 
-  /*card number input*/ 
-  $(document).on('change', '#card-number', function(event) {
-    $(event.target).next().remove();
-    $(event.target).removeClass("error-border")
-    typeOnCard(event.target,$('#on-card-number'))
-    if (/[a-zA-Z`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/.test(event.target.value) ||event.target.value.length < 19) {
-      const errMsg = $('<label>').addClass('error-message').text('Wrong format, number only, full number');
-      $(event.target).after(errMsg);
-      $(event.target).addClass("error-border");
-    }
-   });
-//submit button 
-$("form").on('submit', '#cardholder', function(event) {
-event.preventDefault();
-alert('submitted')
-  });
+});
+/*card number input*/
+$(document).on('change', '#card-number', function (event) {
+  typeOnCard(event.target, $('#on-card-number'))
 
+});
+//card expiary date
+$(document).on('change', '#exp-date-mm', function (event) {
+  typeOnCard(event.target, $('#mm-card'))
 
-   /*experation month */
-   $(document).on('change', '#exp-date-mm', function(event) {
-    typeOnCard(event.target,$('#mm-card'))
-    }
-   );
+});
+$(document).on('change', '#exp-date-yy', function (event) {
+  typeOnCard(event.target, $('#yy-card'))
 
-   //experation year
-   $(document).on('change', '#exp-date-yy', function(event) {
-    typeOnCard(event.target,$('#yy-card'))
- 
-   });
-  //CVC
-   $(document).on('change', '#CVC', function(event) {
-    typeOnCard(event.target,$('#CVC-on-card'))
+});
+//CVC
+$(document).on('change', '#CVC', function (event) {
+  typeOnCard(event.target, $('#CVC-on-card'))
 
-    if (/[a-zA-Z`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/.test(event.target.value)) {
-      alert('please only use numbers');
-    }
-    if(!event.target.value.trim()){
-      alert('cant be blank')
-    }
- 
-   });
-   function typeOnCard(e, where){
-    let text = e.value
-    where.text(text)
+});
+//function to type on card
+function typeOnCard(e, where) {
+  let text = e.value
+  where.text(text)
 }
+//on submit function
+$('form').on("submit", function (event) {
+  event.preventDefault()
+  let check = 0
+  $('input').each(function () {
+    let value = $(this).val();
+    let inputID = $(this).attr('id')
+    $(this).next().remove();
+    $(this).removeClass("error-border");
+    console.log(value);
+    /*error messages for each of the input */
+    switch (inputID) {
+      case "cardholder":
+        if (/[0-9-`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/.test($(this).val()) || !$(this).val()) {
+          const errMsg = $('<label>').addClass('error-message').text('Enter name as on your card');
+          $(this).after(errMsg)
+          $(this).addClass("error-border");
+        } else check++
+        break;
+      case "card-number":
+        if (/[a-zA-Z`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/.test($(this).val()) || $(this).val().length < 19) {
+          const errMsg = $('<label>').addClass('error-message').text('Wrong format, number only, full number');
+          $(this).after(errMsg);
+          $(this).addClass("error-border")
+        }else check++;
+        break;
+      case "CVC":
+        if (!$(this).val()) {
+          const errMsg = $('<label>').addClass('error-message').text("Can't be blank");
+          $(this).after(errMsg);
+          $(this).addClass("error-border");
+        }else if ($(this).val().length !=3) {
+          const errMsg = $('<label>').addClass('error-message').text("Missing number");
+          $(this).after(errMsg);
+          $(this).addClass("error-border");
+        }
+        else check++;
+        break;
+      default:
+        if (!$(this).val()) {
+          const errMsg = $('<label>').addClass('error-message').text("Can't be blank");
+          $(this).after(errMsg);
+          $(this).addClass("error-border");
+        }else check++;
 
+    }
+    console.log(check)
+  });
+
+//creating thankyou page
+  if (check > 4){
+    $('form').remove()
+    
+    const thanks = $('<h3>').text("Thank you!");
+    const saved = $('<h4>').text("We've added your card details");
+    const button = $('<button>').addClass('buttons').text('Continue');
+    $('svg').removeClass('hide')
+    $('#main-inputs').append($('<section>',{'id' : 'thankyou'}).append([thanks,saved,button]))
+  }
+})
 
 
 
